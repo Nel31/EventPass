9e5f587e53571db0601b0b2d6f5e4068f70296a8
Nel31 <ornel.whannou-de-dravo@epitech.eu>
2025-06-04 15:44:20 +0100

    fix: correct server supabase cookie handling

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,4 +1,4 @@
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
@@ -7,12 +7,14 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        async getAll() {
-          return (await cookieStore).getAll()
+        getAll() {
+          return cookieStore.getAll()
         },
-        async setAll(cookiesToSet) {
+        setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(async ({ name, value, options }) => (await cookieStore).set(name, value, options))
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
           } catch {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -21,5 +23,6 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
         },
       },
     },
-  );
-};
+  )
+}
+
